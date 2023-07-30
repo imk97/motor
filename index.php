caa0143ad0d3201cf224033e976bb09ebf1652db
imk97 <itmam97@gmail.com>
2023-07-30 19:12:10 +0800

    create card contain

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-            background-color: #B2B2B2;
+            background-color: #FFFFFF;
         }
 
         .navigationBar {
@@ -22,7 +22,7 @@
 
         .navigationBar > p {
             font-weight: bold;
-            padding: 15px 10px;
+            padding: 15px 20px;
         }
 
         .navigationBar > p:first-child {
@@ -46,6 +46,22 @@
             text-align: center;
         }
 
+        .card-container {
+            padding: 0px 20px;
+        }
+
+        .card-container > h5 {
+            font-weight: bold;
+        }
+
+        .card-contain {
+            width: 100%;
+            height: 100px;
+            background-color: #FFFFFF;
+            border-radius: 0px;
+            border: 1px solid black
+        }
+
     </style>
 </head>
 <body>
@@ -57,6 +73,15 @@
     <div class="header">
         <h2>2 Motor Maintenace</h2>
         <h3>in Scheduled</h3>
+    </div>
+
+    <!-- Contain card -->
+    <div class="card-container">
+        <h5>Select</h5>
+        <div class="card-contain">
+            <p>5 May 2023</p>
+            <h5>Tukar Minyak Hitam</h5>
+        </div>
     </div>
 </body>
 </html>
