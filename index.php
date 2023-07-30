2b8c524dc59457c10a45d59418fc16c821e0c4b1
imk97 <itmam97@gmail.com>
2023-07-30 13:05:47 +0800

    design navigation bar and header

--- index.php
+++ index.php
@@ -11,17 +11,52 @@
             margin: 0;
             padding: 0;
             box-sizing: border-box;
+            background-color: #B2B2B2;
+        }
+
+        .navigationBar {
+            /* background-color: #B2B2B2; */
+            width: 100%;
+            height: 50px;
+        }
+
+        .navigationBar > p {
+            font-weight: bold;
+            padding: 15px 10px;
+        }
+
+        .navigationBar > p:first-child {
+            position: absolute;
+            left: 0;
+        }
+
+        .navigationBar > p:last-child {
+            position: absolute;
+            right: 0;
         }
 
         .header {
-            background-color: #B2B2B2;
             width: 100%;
-            height: 50px;
+            height: 100px;
+            padding: 30px;
+            /* background-color: whitesmoke; */
+        }
+
+        .header > h2, h3 {
+            text-align: center;
         }
 
     </style>
 </head>
 <body>
-    <div class="header"></div>
+    <div class="navigationBar">
+        <p>Hi, itmam</p>
+        <p>&#43;</p>
+    </div>
+
+    <div class="header">
+        <h2>2 Motor Maintenace</h2>
+        <h3>in Scheduled</h3>
+    </div>
 </body>
 </html>
