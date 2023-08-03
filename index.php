ab75253febca12e71d4690e3106a7fe35aa2149e
itmam md khairi <itmam97@gmail.com>
2023-08-03 09:57:40 +0800

    cont update again

--- index.php
+++ index.php
@@ -140,8 +140,8 @@
             position: fixed;
             top: 0;
             left: 0;
-            /* right: 0;
-            bottom: 0; */
+            right: 0;
+            bottom: 0;
             width: 100%;
             height: 100%;
             /* background-color: rgba(0,0,0,0.85);
