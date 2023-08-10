8878fb60474c17457baa4168f8a234b1e22f4eb1
itmam md khairi <itmam97@gmail.com>
2023-08-10 21:47:18 +0800

    sidemenu

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "";
-$username = "";
-$password = "";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$db = "motor";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
